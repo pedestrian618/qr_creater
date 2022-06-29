3219cbc4f3eaae62251542f6caf512a830317eb5
pedestrian <60123345+pedestrian618@users.noreply.github.com>
2022-06-29 16:18:41 +0900

    second commit
    second commit

--- qrapp.py
+++ qrapp.py
@@ -1,14 +1,9 @@
 import streamlit as st
 import pyqrcode as qr
-import cv2
-import time
 from PIL import Image
 
 st.title("QRコード生成器")
 text = st.text_input("テキストを入力して下さい")
-
-cap = cv2.VideoCapture(0)
-font = cv2.FONT_HERSHEY_SIMPLEX
 
 if text:
     qrcode = qr.create(text)
